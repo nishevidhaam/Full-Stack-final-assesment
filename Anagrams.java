c43882a5ed0c62549f5e0adff478321bcb7a7068
nishevidhaam <114397200+nishevidhaam@users.noreply.github.com>
2022-12-09 12:40:00 +0530

    Add files via upload

---
+++ Anagrams.java
@@ -0,0 +1,51 @@
+package Incedo;
+
+
+import java.util.*;
+
+public class Anagrams {
+
+	private static void printAnagrams(String arr[])
+	{
+		HashMap<String, List<String> > map
+			= new HashMap<>();
+
+		for (int i = 0; i < arr.length; i++) {
+
+			
+			String word = arr[i];
+			char[] letters = word.toCharArray();
+			Arrays.sort(letters);
+			String newWord = new String(letters);
+
+			
+			if (map.containsKey(newWord)) {
+
+				map.get(newWord).add(word);
+			}
+			else {
+
+				List<String> words = new ArrayList<>();
+				words.add(word);
+				map.put(newWord, words);
+			}
+		}
+
+		
+		for (String s : map.keySet()) {
+			List<String> values = map.get(s);
+			if (values.size() > 1) {
+				System.out.print(values);
+			}
+		}
+	}
+
+	public static void main(String[] args)
+	{
+
+		
+		String arr[]
+			= { "eat", "tea", "tan", "ate", "nat" };
+		printAnagrams(arr);
+	}
+}
